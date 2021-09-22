52c54b4b67780b9795226ee0be3ff0a08e143f83
devshekhawat <dev.shekhawat.dev@gmail.com>
2021-09-22 17:32:59 +0530

    Test case update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders Cars in Jaipur heading', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Cars in Jaipur/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders filter component', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Filter by Origin/i);
-  expect(linkElement).toBeInTheDocument();
+test('Match App Snapshot', () => {
+  const component = renderer(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
